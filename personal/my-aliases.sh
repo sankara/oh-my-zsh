10c2b159441efcd285281cfb418a905d43ddeb28
Sankara Rameswaran <hello@sankara.me>
2017-04-20 11:44:35 -0700

    Release functions

--- personal/my-aliases.sh
+++ personal/my-aliases.sh
@@ -1,1 +1,3 @@
 alias show-deploy-log='ssh -t p1 "tail -f ~/dolphin-deploy.log"'
+alias create-release='git checkout master && git pull --rebase && git checkout -b $release_branch && for i in $branches; do; git checkout $i; git rebase $release_branch; git rebase $i $release_branch; done;'
+alias cleanup-release='git checkout master && git push origin :$release_branch && git branch -D $release_branch && for i in $branches; do; git br -D $i; git push origin :$i; done;'
