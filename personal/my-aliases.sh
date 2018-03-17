2d1ec15802b6609b3d6bd958510b2f6a668defc0
Sankara Rameswaran <hello@sankara.me>
2018-03-16 20:04:48 -0700

    Another alias

--- personal/my-aliases.sh
+++ personal/my-aliases.sh
@@ -1,3 +1,4 @@
 alias show-deploy-log='ssh -t p1 "tail -f ~/dolphin-deploy.log"'
 alias create-release='git checkout master && git pull --rebase && git checkout -b $release_branch && for i in $branches; do; git checkout $i; git rebase $release_branch; git rebase $i $release_branch; done;'
 alias cleanup-release='git checkout master && git push origin :$release_branch && git branch -D $release_branch && for i in $branches; do; git br -D $i; git push origin :$i; done;'
+alias cdh='cd /mnt/c/Users/Sankara/Workplace'
